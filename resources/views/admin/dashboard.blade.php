ad28fdaaa903a10eca4588e31abb309bfac161f3
gaetano-94 <gaetano.panico1994@gmail.com>
2024-02-07 18:50:06 +0100

    feat: table style projects

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -5,7 +5,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8 mt-4">
                 <div class="card">
-                    <div class="card-header">{{ __('Dashboard') }}</div>
+                    <div class="card-header">{{ __('La mia Dashboard') }}</div>
 
                     <div class="card-body">
                         @if (session('status'))
@@ -14,7 +14,7 @@
                             </div>
                         @endif
 
-                        {{ __('You are logged in!') }}
+                        {{ __('Sei Loggato!') }}
                     </div>
                 </div>
             </div>
--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -2,9 +2,33 @@
 
 @section('content')
     <h1>Elenco dei Progetti</h1>
+    <table class="table">
+        <thead>
+            <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Titolo</th>
+                <th scope="col">Slug</th>
+                <th scope="col"></th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach ($projects as $project)
+                <tr>
+                    <th>{{ $project->id }}</th>
+                    <td>{{ $project->title }}</td>
+                    <td>{{ $project->slug }}</td>
+                    <td>
+                        <a href="" role="button" class="btn btn-primary btn-sm">Crea</a>
+                        <a href="" role="button" class="btn btn-info btn-sm">Dettagli</a>
+                        <a href="" role="button" class="btn btn-danger btn-sm">Elimina</a>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
     <ul>
-        @foreach ($projects as $project)
-            <li>{{ $project->title }}</li>
-        @endforeach
+
+        <li>{{ $project->title }}</li>
+
     </ul>
 @endsection
